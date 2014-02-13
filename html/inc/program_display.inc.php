2d87e511f3113cc1ac3d31001538d781077274c4
benpeacock <benjaminwpeacock@gmail.com>
2014-02-13 09:57:27 -0600

    Added nl2br() to preserver para formatting in answer display

--- html/inc/program_display.inc.php
+++ html/inc/program_display.inc.php
@@ -11,19 +11,20 @@ foreach ($tabs as $tab) {
 				echo '<div class="row marginbottom20">';
 				echo '<label class="col-sm-3 control-label">' . $question['question'] . '</label>';
 				echo '<div class="col-sm-9">';
-				echo $answer->answer;
+				echo nl2br($answer->answer);
 				echo '</div>';
 				echo '</div>';
 				break;
 			case (2):
 				echo '<label>' . $question['question'] . '</label>';
-				echo '<div class="marginbottom20 col-sm-12">' . $answer->answer . '</div>';
+				echo '<div class="marginbottom20 
+col-sm-12">' . nl2br($answer->answer) . '</div>';
 				break;
 			case (3):
 				echo '<div class="row marginbottom20">';
 				echo '<label class="col-sm-3 control-label">' . $question['question'] . '</label>';
 				echo '<div class="col-sm-9">';
-				echo $answer->answer;
+				echo nl2br($answer->answer);
 				echo '</div>';
 				echo '</div>';
 				break;
